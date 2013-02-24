5c619035581cbe77d3919d762f540043bab296b9
Alek Monstovich <alek.monstovich@gmail.com>
2013-02-24 18:05:08 +0400

    Change output for prompt

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,115 +1,125 @@
-txtund=$(tput sgr 0 1)
-txtbld=$(tput bold)
+# setup color variables
+color_is_on=
+color_red=
+color_green=
+color_yellow=
+color_blue=
+color_white=
+color_gray=
+color_bg_red=
+color_off=
+color_user=
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    color_is_on=true
+    color_red="\[$(/usr/bin/tput setaf 1)\]"
+    color_green="\[$(/usr/bin/tput setaf 2)\]"
+    color_yellow="\[$(/usr/bin/tput setaf 3)\]"
+    color_blue="\[$(/usr/bin/tput setaf 6)\]"
+    color_white="\[$(/usr/bin/tput setaf 7)\]"
+    color_gray="\[$(/usr/bin/tput setaf 8)\]"
+    color_off="\[$(/usr/bin/tput sgr0)\]"
 
-reset='\[\e[00m\]'
-bold='\[\e[01m\]'
-red='\[\e[31m\]'
-green='\[\e[32m\]'
-orange='\[\e[33m\]'
-blue='\[\e[34m\]'
-purple='\[\e[35m\]'
-cyan='\[\e[36m\]'
-gray='\[\e[37m\]'
-bright='\[\e[39m\]'
+    color_error="$(/usr/bin/tput setab 1)$(/usr/bin/tput setaf 7)"
+    color_error_off="$(/usr/bin/tput sgr0)"
 
-function _get_prompt() {
-  local git_status="`git status -unormal 2>&1`"
-  if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
-    if [[ "$git_status" =~ nothing\ to\ commit ]]; then
-      local ansi=42
-    elif [[ "$git_status" =~ nothing\ added\ to\ commit\ but\ untracked\ files\ present ]]; then
-      local ansi=43
-    else
-      local ansi=45
-    fi
-    if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
-      branch=${BASH_REMATCH[1]}
-      test "$branch" != master || branch=' '
-    else
-      # Detached HEAD.  (branch=HEAD is a faster alternative.)
-      branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null || echo HEAD`)"
-    fi
-    echo -n '\[\e[0;37;'"$ansi"';1m\]'"$branch"'\[\e[0m\] '
-  fi
-}
-
-function _prompt_command() {
-  PS1="`_git_prompt`"'$green\u@$blue\h$\$reset '
-}
-
-function short_pwd {
-  pwd=$(pwd)
-  first=$(basename $pwd)
-  rest=$(dirname $pwd)
-  second=$(basename $rest)
-  rest=$(dirname $rest)
-  third=$(basename $rest)
-  if [ $second = "/" ]; then
-    echo -n "/$first"
-  elif [ $third = "/" ]; then
-    echo -n "/$second/$first"
-  else
-    echo -n "$third/$second/$first"
-  fi
-}
-
-function myprompt {
-  git_top=$(git rev-parse --show-toplevel 2>/dev/null)
-
-  if [ $? -ne 0 ]; then
-    git_top=""
-    GIT=0
-  else
-    git_top=$(basename $git_top)
-    GIT=1
-  fi
-
-  if [ $GIT -ne 0 ]; then
-    git_branch=$(git branch | grep "*" | awk '{print $2}')
-    if [ $? -ne 0 ]; then
-      git_branch=""
-    fi
-
-    if [ $git_branch = "master" ]; then
-      branch="master"
-    else
-      branch=" $bold$red($git_branch)$reset"
-    fi
-  fi
-
-  colordir="${bold}${blue}$(short_pwd)"
-  if [ $GIT -ne 0 ]; then
-    # We're in a git repo
-    EDITS=$(git status -e | wc -l)
-    if [ $EDITS -ne 0 ]; then
-      edits=" $bold$orange($EDITS)$reset"
-    else
-      edits=""
-    fi
-    AHEAD=$(git rev-list @{u}..HEAD 2>/dev/null | wc -l)
-    BEHIND=$(git rev-list HEAD..@{u} 2>/dev/null | wc -l)
-    if [ $AHEAD -ne 0 -a $BEHIND -eq 0 ]; then
-      ahead_behind="$bold$red +$AHEAD"
-    elif [ $BEHIND -ne 0 -a $AHEAD -eq 0 ]; then
-      ahead_behind="$bold$red -$BEHIND"
-    elif [ $AHEAD -eq 0 -a $BEHIND -eq 0 ]; then
-      ahead_behind=""
-    else
-      ahead_behind="$bold$red +$AHEAD -$BEHIND"
-    fi
-    git="${bold}${green}$git_top$edits$ahead_behind"
-    PS1="$git$branch $colordir$reset\$ "
-  else
-    PS1="$colordir$reset\$ "
-  fi
-  if [ $VIRTUAL_ENV ]; then
-    PS1="$bold$cyan$(basename $VIRTUAL_ENV)$reset $PS1"
-  fi
-}
-
-unset PS1
-if [ $user = 'tumodsru' ]; then
-  PROMPT_COMMAND=mypromp
-else
-  PS1='\u@\h$ '
+    # set user color
+    case `id -u` in
+        0) color_user=$color_red ;;
+        *) color_user=$color_green ;;
+    esac
 fi
+
+# some kind of optimization - check if git installed only on config load
+PS1_GIT_BIN=$(which git 2>/dev/null)
+
+function prompt_command {
+    local PS1_GIT=
+    local PS1_VENV=
+    local GIT_BRANCH=
+    local GIT_DIRTY=
+    local PWDNAME=$PWD
+
+    # beautify working directory name
+    if [[ "${HOME}" == "${PWD}" ]]; then
+        PWDNAME="~"
+    elif [[ "${HOME}" == "${PWD:0:${#HOME}}" ]]; then
+        PWDNAME="~${PWD:${#HOME}}"
+    fi
+
+    # parse git status and get git variables
+    if [[ ! -z $PS1_GIT_BIN ]]; then
+        # check we are in git repo
+        local CUR_DIR=$PWD
+        while [[ ! -d "${CUR_DIR}/.git" ]] && [[ ! "${CUR_DIR}" == "/" ]] && [[ ! "${CUR_DIR}" == "~" ]] && [[ ! "${CUR_DIR}" == "" ]]; do CUR_DIR=${CUR_DIR%/*}; done
+        if [[ -d "${CUR_DIR}/.git" ]]; then
+            # 'git repo for dotfiles' fix: show git status only in home dir and other git repos
+            if [[ "${CUR_DIR}" != "${HOME}" ]] || [[ "${PWD}" == "${HOME}" ]]; then
+                # get git branch
+                GIT_BRANCH=$($PS1_GIT_BIN symbolic-ref HEAD 2>/dev/null)
+                if [[ ! -z $GIT_BRANCH ]]; then
+                    GIT_BRANCH=${GIT_BRANCH#refs/heads/}
+
+                    # get git status
+                    local GIT_STATUS=$($PS1_GIT_BIN status --porcelain 2>/dev/null)
+                    [[ -n $GIT_STATUS ]] && GIT_DIRTY=1
+                fi
+            fi
+        fi
+    fi
+
+    # build b/w prompt for git and virtual env
+    [[ ! -z $GIT_BRANCH ]] && PS1_GIT=" (git: ${GIT_BRANCH})"
+    [[ ! -z $VIRTUAL_ENV ]] && PS1_VENV=" (venv: ${VIRTUAL_ENV#$WORKON_HOME})"
+
+    # calculate prompt length
+    local PS1_length=$((${#USER}+${#HOSTNAME}+${#PWDNAME}+${#PS1_GIT}+${#PS1_VENV}+3))
+    local FILL=
+
+    # if length is greater, than terminal width
+    if [[ $PS1_length -gt $COLUMNS ]]; then
+        # strip working directory name
+        PWDNAME="...${PWDNAME:$(($PS1_length-$COLUMNS+3))}"
+    else
+        # else calculate fillsize
+        local fillsize=$(($COLUMNS-$PS1_length))
+        FILL=$color_gray
+        while [[ $fillsize -gt 0 ]]; do FILL="${FILL}─"; fillsize=$(($fillsize-1)); done
+        FILL="${FILL}${color_off}"
+    fi
+
+    if $color_is_on; then
+        # build git status for prompt
+        if [[ ! -z $GIT_BRANCH ]]; then
+            if [[ -z $GIT_DIRTY ]]; then
+                PS1_GIT=" (git: ${color_green}${GIT_BRANCH}${color_off})"
+            else
+                PS1_GIT=" (git: ${color_red}${GIT_BRANCH}${color_off})"
+            fi
+        fi
+
+        # build python venv status for prompt
+        [[ ! -z $VIRTUAL_ENV ]] && PS1_VENV=" (venv: ${color_blue}${VIRTUAL_ENV#$WORKON_HOME}${color_off})"
+    fi
+
+    # set new color prompt
+    PS1="${color_user}${USER}${color_off}@${color_yellow}${HOSTNAME}${color_off}:${color_white}${PWDNAME}${color_off}${PS1_GIT}${PS1_VENV} ${FILL}\n➜ "
+
+    # get cursor position and add new line if we're not in first column
+    # cool'n'dirty trick (http://stackoverflow.com/a/2575525/1164595)
+    # XXX FIXME: this hack broke ssh =(
+#	exec < /dev/tty
+#	local OLDSTTY=$(stty -g)
+#	stty raw -echo min 0
+#	echo -en "\033[6n" > /dev/tty && read -sdR CURPOS
+#	stty $OLDSTTY
+    echo -en "\033[6n" && read -sdR CURPOS
+    [[ ${CURPOS##*;} -gt 1 ]] && echo "${color_error}↵${color_error_off}"
+
+    # set title
+    echo -ne "\033]0;${USER}@${HOSTNAME}:${PWDNAME}"; echo -ne "\007"
+}
+
+# set prompt command (title update and color prompt)
+PROMPT_COMMAND=prompt_command
+# set new b/w prompt (will be overwritten in 'prompt_command' later for color prompt)
+PS1='\u@\h:\w\$ '
